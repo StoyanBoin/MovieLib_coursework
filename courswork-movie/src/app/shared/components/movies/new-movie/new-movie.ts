ab3b4756bdad94562156314d17797954ed94f30b
Stoyan Boin <stoyan.boin@gmail.com>
2026-04-18 11:03:21 +0300

    add form to new-movie

--- courswork-movie/src/app/shared/components/movies/new-movie/new-movie.ts
+++ courswork-movie/src/app/shared/components/movies/new-movie/new-movie.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { form } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-new-movie',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './new-movie.html',
   styleUrl: './new-movie.css',
 })
-export class NewMovie {}
+export class NewMovie {
+  movieName = '';
+  subscribers = '';
+  imageUrl = '';
+
+  private router = inject(Router);
+
+  onSubmit(): void {
+    console.log('New Movie:', this.movieName, this.subscribers, this.imageUrl);
+    this.router.navigate(['/movies']);
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/']);
+  }
+}
