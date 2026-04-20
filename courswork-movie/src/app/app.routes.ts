446b2519f7d3a6524df2d8fa318a3b14ae1d56ec
Stoyan Boin <stoyan.boin@gmail.com>
2026-04-20 21:47:24 +0300

    fix

--- courswork-movie/src/app/app.routes.ts
+++ courswork-movie/src/app/app.routes.ts
@@ -5,7 +5,6 @@ export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./features/home/home').then(m => m.Home) },
     { path: 'movies', loadComponent: () => import('./shared/components/movies/movies').then(m => m.Movies) },
-    { path: 'movie-list', redirectTo: 'movies', pathMatch: 'full' },
     { path: 'new-movie', loadComponent: () => import('./shared/components/movies/new-movie/new-movie').then(m => m.NewMovie), canActivate: [authGuard] },
     { path: 'favorites', loadComponent: () => import('./shared/components/favorites/favorites').then(m => m.Favorites), canActivate: [authGuard] },
     { path: 'profile', loadComponent: () => import('./features/profile/profile').then(m => m.Profile), canActivate: [authGuard] },
--- courswork-movie/src/app/features/home/home.ts
+++ courswork-movie/src/app/features/home/home.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [MovieList, RecentPost, RouterLink],
+  imports: [ RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
